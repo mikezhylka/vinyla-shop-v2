88c8762f28350a4b3e3cc55fb7bc6f009e589cc6
mikezhylka <mikezhylka@users.noreply.github.com>
2026-08-02 11:25:04 +0200

    fix: update backend url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
--- apps/client/src/shared/api/base-url.ts
+++ apps/client/src/shared/api/base-url.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+import process from "process";
+
+export const BASE_URL = process.env.BASE_URL;
